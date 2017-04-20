1c7af9bb46559fda18a93c8454b2b423f421a9be
howdy39 <tatsuya.howdy@gmail.com>
2017-04-20 17:05:08 +0900

    add controllers

---
+++ app/main.go
@@ -0,0 +1,11 @@
+package backend
+
+import (
+	"net/http"
+	"verify-gae-feature/src/controllers"
+)
+
+func init() {
+	http.HandleFunc("/", controllers.Root)
+	http.HandleFunc("/appengine", controllers.AppenginePackage)
+}
---
+++ src/controllers/root.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"html/template"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+)
+
+// Root /のコントローラ
+func Root(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	log.Debugf(c, "appID: %v", appengine.AppID(c))
+
+	if err := rooteTemplate.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var rooteTemplate = template.Must(template.New("root").Parse(`
+<html>
+  <head>
+    <title>verify gae feature</title>
+  </head>
+  <body>
+  	<ul>
+	  <li>
+	  	<a href="./appengine">appengine</a>
+	  </li>
+	</ul>
+  </body>
+</html>
+`))
